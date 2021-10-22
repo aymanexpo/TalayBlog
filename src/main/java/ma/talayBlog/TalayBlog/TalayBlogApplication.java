2ea3857e662481dc8ff0c689cbae4ddf3b992cb9
Aymane Talibi <aymane.talibi.at@gmail.com>
2021-10-22 20:31:50 +0100

    Blog App
    a blog App using Stpring boot and Angular

---
+++ src/main/java/ma/talayBlog/TalayBlog/TalayBlogApplication.java
@@ -0,0 +1,13 @@
+package ma.talayBlog.TalayBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalayBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalayBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/talayBlog/TalayBlog/TalayBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.talayBlog.TalayBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalayBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
